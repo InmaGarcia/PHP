c21f105c4158f668570b5ace4620cb503924937b
InmaGarcia <151220236+InmaGarcia@users.noreply.github.com>
2025-02-05 16:17:15 +0100

    Delete Cerveceria2 directory

--- Cerveceria2/componentes/cierreSesion.php
+++
@@ -1,7 +0,0 @@
-<?php
-//cojo la sesion y la cierro
-    session_start();
-    echo "<script>alert('Has cerrado la sesión {$_SESSION['email']}');</script>";
-    session_destroy();
-    echo "<script>window.location= './index.php'</script>";
-?>
